4d2a3da0f790be319ab6b102499242c99aede688
pojun406 <pojun406@naver.com>
2020-12-06 05:18:13 +0900

    Book_management
    도서관리 업데이트

--- Book_Management/DataBase.cs
+++ Book_Management/DataBase.cs
@@ -11,5 +11,17 @@ namespace Book_Management
     class DataBase
     {
         public static SqlConnection conn = new SqlConnection();
+        public static DataSet ds = new DataSet();
+        public static DataTable dt_book;
+        public static DataTable dt_user;
+
+        public static void ConnectDB()
+        {
+            conn.ConnectionString = string.Format("Data Source=127.0.0.1,3307;Initial Catalog=book_management;User ID=root;Password=123123;");
+            conn = new SqlConnection(conn.ConnectionString);
+            conn.Open();
+        }
+
+        public static void 
     }
 }
