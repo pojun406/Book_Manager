175874457c9976b73b82f3886a616360832f203d
pojun406 <pojun406@naver.com>
2020-12-09 00:36:58 +0900

    도서관리 프로젝트
    도서관리 프로젝트 업로드

--- Book_Management/LoginForm.cs
+++ Book_Management/LoginForm.cs
@@ -25,11 +25,11 @@ namespace ProgramForm
 
         private void Button_Login_Click(object sender, EventArgs e)
         {
-            string myConnection = "Server=localhost; port = 3307; Database=book_management;User ID=root;Password=123123;CHARSET=utf8";
+            string myConnection = "Server=localhost; port = 3307; Database=book_management;User ID=root;Password=123123;CHARSET=utf8"; // DB정보
             MySqlConnection Conn = new MySqlConnection(myConnection);
             string query = "select * from user where USER_NAME = '" + txt_ID.Text.Trim() + "' and USER_P_NUM = '" + txt_PW.Text.Trim() + "'";
-            MySqlDataAdapter adpt = new MySqlDataAdapter(query, Conn);
-            DataTable dt = new DataTable();
+            MySqlDataAdapter adpt = new MySqlDataAdapter(query, Conn); // adapter로 쿼리 넘김
+            DataTable dt = new DataTable(); // dataTable 선언
             Conn.Open();
             adpt.Fill(dt);
             if (txt_ID.Text == "admin" && txt_PW.Text == "1234")
@@ -42,7 +42,7 @@ namespace ProgramForm
             }
             else
             {
-                if (dt.Rows.Count == 1)
+                if (dt.Rows.Count == 1) // 연결된 개수가 1개라면 실행 ( 로그인 성공시 1개 실패시 0개 )
                 {
                     MessageBox.Show("로그인합니다.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
                     UserForm userForm = new UserForm();
