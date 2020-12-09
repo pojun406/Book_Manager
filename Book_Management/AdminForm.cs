4c441d6a36d110f27bbe1417de4970ef301fc62d
pojun406 <pojun406@naver.com>
2020-12-10 07:31:58 +0900

    도서관리 프로젝트
    도서관리 프로젝트 업로드

--- Book_Management/AdminForm.cs
+++ Book_Management/AdminForm.cs
@@ -13,7 +13,6 @@ namespace ProgramForm
 {
     public partial class AdminForm : Form
     {
-        private ListBox listBox1;
         public AdminForm()
         {
             InitializeComponent();
@@ -39,12 +38,11 @@ namespace ProgramForm
             
         }
 
-        private void btnMake_Click(object sender, EventArgs e)
+        private void btnMake_Click(object sender, EventArgs e) // 유저 회원가입버튼
         {
             string myConnection = "Server=localhost; port = 3307; Database=book_management; User ID=root;Password=123123;CHARSET=utf8"; // DB정보
             MySqlConnection Conn = new MySqlConnection(myConnection);
-            string query = "INSERT INTO user (USER_NUM, USER_NAME, USER_P_NUM) value(USER_NUM + 1, '" + txt_Name.Text.Trim() + "' , '" + txt_Tel.Text.Trim() + "');";
-
+            string query = "INSERT INTO user (USER_NUM, USER_NAME, USER_P_NUM) value(USER_NUM, '" + txt_Name.Text.Trim() + "' , '" + txt_Tel.Text.Trim() + "');";
             Conn.Open();
             MySqlCommand cmd = new MySqlCommand(query, Conn);
 
@@ -52,7 +50,7 @@ namespace ProgramForm
             {
                 if(cmd.ExecuteNonQuery() == 1)
                 {
-                    MessageBox.Show("값 들어감");
+                    MessageBox.Show("유저 아이디 : " + txt_Name.Text + "   유저 핸드폰번호 : " + txt_Tel.Text);
                 }
                 else
                 {
@@ -99,6 +97,38 @@ namespace ProgramForm
         }
 
         private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_AddBook_Click(object sender, EventArgs e) // 도서 추가 버튼
+        {
+            string myConnection = "Server=localhost; port = 3307; Database=book_management; User ID=root;Password=123123;CHARSET=utf8"; // DB정보
+            MySqlConnection Conn = new MySqlConnection(myConnection);
+            string query = "INSERT INTO book_list (BOOK_NUM, BOOK_NAME, BOOK_GENRE, BOOK_WRITER) value('" + txt_booknum.Text.Trim() + "', '" + txt_bookname.Text.Trim() + "' , '" + txt_genre.Text.Trim() + "' , '" + txt_writer.Text.Trim() + "');";
+            Conn.Open();
+            MySqlCommand cmd = new MySqlCommand(query, Conn);
+
+            try
+            {
+                if (cmd.ExecuteNonQuery() == 1)
+                {
+                    MessageBox.Show("도서 번호 : " + txt_booknum.Text + "\r 도서 제목 : " + txt_bookname.Text + "\r 도서 장르 : " + txt_genre + "\r 도서 저자 : " + txt_writer);
+                }
+                else
+                {
+                    MessageBox.Show("값 들어가기 실패");
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+
+            Conn.Close();
+        }
+
+        private void btn_DelBook_Click(object sender, EventArgs e) // 도서 삭제 버튼
         {
 
         }
